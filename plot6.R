da640d5210f7c72e1e33458488b0d90c40406631
kannanthegreat <kannanthegreat@gmail.com>
2016-08-08 12:41:36 +0530

    Add files via upload
    Plot 6 code

---
+++ plot6.R
@@ -0,0 +1,23 @@
+# Exploratory Data Analysis - Assignment 2 - Q. #6
+# Load ggplot2 library
+library(ggplot2)
+
+# Loading provided datasets - loading from local machine
+NEI <- readRDS("C:/Users/kannanthegreat/Documents/EDA/summarySCC_PM25.rds")
+SCC <- readRDS("C:/Users/kannanthegreat/Documents/EDA/Source_Classification_Code.rds")
+
+subsetNEI <- NEI[(NEI$fips=="24510"|NEI$fips=="06037") & NEI$type=="ON-ROAD",  ]
+
+aggregatedTotalByYearAndFips <- aggregate(Emissions ~ year + fips, subsetNEI, sum)
+aggregatedTotalByYearAndFips$fips[aggregatedTotalByYearAndFips$fips=="24510"] <- "Baltimore, MD"
+aggregatedTotalByYearAndFips$fips[aggregatedTotalByYearAndFips$fips=="06037"] <- "Los Angeles, CA"
+
+png("C:/Users/kannanthegreat/Documents/EDA/plot6.png", width=1040, height=480)
+g <- ggplot(aggregatedTotalByYearAndFips, aes(factor(year), Emissions))
+g <- g + facet_grid(. ~ fips)
+g <- g + geom_bar(stat="identity")  +
+        xlab("year") +
+        ylab(expression('Total PM'[2.5]*" Emissions")) +
+        ggtitle('Total Emissions from motor vehicle (type=ON-ROAD) in Baltimore City, MD (fips = "24510") vs Los Angeles, CA (fips = "06037")  1999-2008')
+print(g)
+dev.off()
